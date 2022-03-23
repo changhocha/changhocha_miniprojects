e57f8fdb589a0309e49e92c1142ffd26dfafd815
changhocha <chachangho45@gmail.com>
2022-03-23 16:43:31 +0900

    fourth commit

--- rock-scissor-paper-react/src/App.js
+++ rock-scissor-paper-react/src/App.js
@@ -5,14 +5,15 @@ import Scoreboard from "./components/Scoreboard";
 
 function App() {
   return (
-    <main>
-      <div className="container">
-        <Header title="가위 바위 보" />
-        <Scoreboard />
-        <Game />
-        <Computerboard cbtitle="컴퓨터 선택" />
-      </div>
-    </main>
+    <body>
+      <main>
+        <div className="container">
+          <Header title="가위 바위 보" />
+          <Scoreboard />
+          <Game />
+        </div>
+      </main>
+    </body>
   );
 }
 
--- rock-scissor-paper-react/src/components/Game.js
+++ rock-scissor-paper-react/src/components/Game.js
@@ -1,26 +1,46 @@
 import React, { useState } from "react";
 import Gameboard from "./Gameboard";
+import Computerboard from "./Computerboard";
 
 const Game = () => {
-  const [p1click, setp1Click] = useState("");
-  const getp1Click = (p1click) => {
-    setp1Click(p1click);
+  const [p1Click, setp1Click] = useState("");
+
+  if (p1Click === "p1Rock") {
+    const playerChoice = "Rock";
+    console.log(playerChoice);
+  } else if (p1Click === "p1Paper") {
+    const playerChoice = "Paper";
+    console.log(playerChoice);
+  } else if (p1Click === "p1Scissors") {
+    const playerChoice = "Scissors";
+    console.log(playerChoice);
+  }
+
+  const handleClick = (p1Selected) => () => {
+    setp1Click(p1Selected);
   };
 
-  if (p1click === "p1Rock") {
-    const playerSetected = "Rock";
-    console.log(playerSetected);
-  } else if (p1click === "p1Paper") {
-    const playerSelected = "Paper";
-    console.log(playerSelected);
-  } else if (p1click === "p1Scissors") {
-    const playerSelected = "Scissors";
-    console.log(playerSelected);
-  }
+  const getComputerChoice = () => {
+    const randomNumber = Math.floor(Math.random() * 3);
+    const rpsArray = ["cpRock", "cpPaper", "cpScissors"];
+    return rpsArray[randomNumber];
+  };
+
+  const determineWinner = (playerChoice, computerChoice) => {};
+
+  const finishGame = () => {
+    const computerChoice = getcomputerChoice();
+    const winner = determineWinner(playerChoice, computerChoice);
+  };
 
   return (
     <div>
-      <Gameboard p1click={p1click} getp1Click={getp1Click} />
+      <Gameboard
+        p1Click={p1Click}
+        onClick={handleClick}
+        gbtitle="플레이어 선택"
+      />
+      <Computerboard cbtitle="컴퓨터 선택" />
     </div>
   );
 };
