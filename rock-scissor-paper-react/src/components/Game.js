2292b047cb418e74f336c197b1a3ef0b17c6ea0c
changhocha <chachangho45@gmail.com>
2022-03-24 22:29:17 +0900

    HELP

--- rock-scissor-paper-react/src/components/Game.js
+++ rock-scissor-paper-react/src/components/Game.js
@@ -4,7 +4,6 @@ import Computerboard from "./Computerboard";
 
 const Game = () => {
   const [p1Click, setp1Click] = useState("");
-<<<<<<< HEAD
   const [p1Choice, setp1Choice] = useState("");
   const [CPUchoice, setCPUchoice] = useState("");
   const [winner, setWinner] = useState("");
@@ -18,31 +17,15 @@ const Game = () => {
   } else if (p1Click === "p1Scissors") {
     setp1Choice("Scissors");
     console.log(p1Choice);
-=======
-
-  if (p1Click === "p1Rock") {
-    const playerChoice = "Rock";
-    console.log(playerChoice);
-  } else if (p1Click === "p1Paper") {
-    const playerChoice = "Paper";
-    console.log(playerChoice);
-  } else if (p1Click === "p1Scissors") {
-    const playerChoice = "Scissors";
-    console.log(playerChoice);
->>>>>>> e57f8fdb589a0309e49e92c1142ffd26dfafd815
   }
 
   const handleClick = (p1Selected) => () => {
     setp1Click(p1Selected);
-<<<<<<< HEAD
     getComputerChoice();
-=======
->>>>>>> e57f8fdb589a0309e49e92c1142ffd26dfafd815
   };
 
   const getComputerChoice = () => {
     const randomNumber = Math.floor(Math.random() * 3);
-<<<<<<< HEAD
     const rpsArray = ["Rock", "Paper", "Scissors"];
     setCPUchoice(rpsArray[randomNumber]);
     console.log(CPUchoice);
@@ -61,28 +44,13 @@ const Game = () => {
     } else {
       setWinner("Player");
     }
-=======
-    const rpsArray = ["cpRock", "cpPaper", "cpScissors"];
-    return rpsArray[randomNumber];
-  };
-
-  const determineWinner = (playerChoice, computerChoice) => {};
-
-  const finishGame = () => {
-    const computerChoice = getcomputerChoice();
-    const winner = determineWinner(playerChoice, computerChoice);
->>>>>>> e57f8fdb589a0309e49e92c1142ffd26dfafd815
   };
 
   return (
     <div>
       <Gameboard
         p1Click={p1Click}
-<<<<<<< HEAD
         onClick={() => handleClick}
-=======
-        onClick={handleClick}
->>>>>>> e57f8fdb589a0309e49e92c1142ffd26dfafd815
         gbtitle="플레이어 선택"
       />
       <Computerboard cbtitle="컴퓨터 선택" />
