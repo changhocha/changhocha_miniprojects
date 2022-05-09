2897bdbf66f5b6dca9ebb1a149e7a48cd8eca6c7
changhocha <chachangho45@gmail.com>
2022-05-09 18:00:30 +0900

    newRPS

--- rps-v2/src/Components/Gameboard.js
+++ rps-v2/src/Components/Gameboard.js
@@ -3,10 +3,17 @@ import Choice from "./Choice";
 
 const Gameboard = () => {
   return (
-    <section className="playerBoard">
-      <Choice choiceName={"rock"} />
-      <Choice choiceName={"paper"} />
-      <Choice choiceName={"scissors"} />
+    <section className="gameBoard">
+      <section className="playerBoard">
+        <Choice choiceName={"rock"} />
+        <Choice choiceName={"paper"} />
+        <Choice choiceName={"scissors"} />
+      </section>
+      <section className="cpuBoard">
+        <Choice choiceName={"rock"} />
+        <Choice choiceName={"paper"} />
+        <Choice choiceName={"scissors"} />
+      </section>
     </section>
   );
 };
